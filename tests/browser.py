6ab2d824e3b1b1bac7de9310050149b59b0b38d4
Himanshu <hsethi@bgsu.edu>
2020-06-09 15:10:53 -0400

    Remove extra return

--- tests/browser.py
+++ tests/browser.py
@@ -6,5 +6,5 @@ from selenium import webdriver
 @pytest.mark.usefixtures("driverSetup")
 class browser():
     def browser(self):
-        return self.driver.get("http://automationpractice.com/index.php")
+        self.driver.get("http://automationpractice.com/index.php")
         return self.driver.maximize_window()
