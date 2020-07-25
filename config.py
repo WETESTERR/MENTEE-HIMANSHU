66bf35674b275705b8f3fefce53f205ad196830f
Himanshu <hsethi@bgsu.edu>
2020-07-25 19:37:23 -0400

    "Updated with JSON data"

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 url = "http://automationpractice.com/index.php"
-project_root = os.getcwd()
+project_root = os.path.dirname(os.path.abspath(''))
 logs_path = project_root+"\\logs"
 wait_time = 10
 sleep_time = 5
--- page_objects/item_search.py
+++ page_objects/item_search.py
@@ -1,16 +1,26 @@
+import json
+import os
+
 from page_objects.common import Common
 from utilities import driver
 from utilities.locator_strategy import LocatorStrategy
+import config
 
 class Search_Item(Common):
+
+
 
     def __init__(self,driver):
         super().__init__(driver)
         self.driver = driver
 
+
+
     search_field = LocatorStrategy.locator_by_id("search_query_top")
     search_button = LocatorStrategy.locator_by_xpath("//button[@name='submit_search']")
 
     def item_search(self):
-        self.enter_text(Search_Item.search_field, text="Printed Dress")
+        with open(config.project_root + '\\json_utilities\\data.json') as f:
+            data_file = json.loads(f.read())
+        self.enter_text(Search_Item.search_field, text=data_file['item'])
         self.click(Search_Item.search_button)
