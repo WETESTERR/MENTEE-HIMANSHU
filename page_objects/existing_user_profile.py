d0738899515eabd93dee8a06dda7942508e703ab
Himanshu <hsethi@bgsu.edu>
2020-09-08 20:59:00 -0400

    "Created the Existing User test script"

--- page_objects/existing_user_profile.py
+++ page_objects/existing_user_profile.py
@@ -1,3 +1,4 @@
+import config
 from page_objects.common import Common
 from utilities.data_factory import DataRead
 from utilities.locator_strategy import LocatorStrategy
@@ -5,11 +6,10 @@ from utilities.locator_strategy import LocatorStrategy
 
 class ExistingUser(Common):
 
-    def __init__(self,driver):
+    def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
         self.data = DataRead.json_read('data.json')
-
 
     login_button = LocatorStrategy.locator_by_class_name('login')
     email_field = LocatorStrategy.locator_by_id("email_create")
@@ -18,9 +18,9 @@ class ExistingUser(Common):
     def login_click(self):
         self.click(ExistingUser.login_button)
 
-
     def enter_email(self):
-        self.enter_text(ExistingUser.email_field,text=self.data['existing_email'])
+        self.enter_text(ExistingUser.email_field, text=self.data['existing_email'])
         self.click(ExistingUser.submit_button)
         self.log.info("Entered email is {}".format(self.data['existing_email']))
+        self.time_sleep(config.sleep_time)
         self.get_screenshot(file_name="Existing_User")
