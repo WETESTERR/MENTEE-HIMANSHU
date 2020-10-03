3596984cb07092d6dc34c81caa3d3f1a38ead904
Himanshu <hsethi@bgsu.edu>
2020-10-03 14:04:01 -0400

    "Tested the slider range test script. Updated the Common methods with comments"

--- page_objects/women_tab.py
+++ page_objects/women_tab.py
@@ -10,10 +10,16 @@ class Womentab(Common):
         self.driver=driver
 
     women_tab = LocatorStrategy.locator_by_xpath("//div[@id='block_top_menu']/ul/li[1]")
-    left_slider = LocatorStrategy.locator_by_css_selector("#layered_price_slider > a:nth-child(2)")
+    left_slider = LocatorStrategy.locator_by_xpath("//a[contains(@class,'ui-slider-handle')][1]")
+    right_slider = LocatorStrategy.locator_by_xpath("//a[contains(@class,'ui-slider-handle')][2]")
+    size_checkbox = LocatorStrategy.locator_by_id("layered_id_attribute_group_3")
 
 
     def slider_range(self):
         self.click(Womentab.women_tab)
         self.time_sleep(config.sleep_time)
-        self.drag_drop_offset(Womentab.left_slider,150,100)
+        self.click(Womentab.size_checkbox)
+        self.time_sleep(config.sleep_time)
+        self.drag_drop_offset(Womentab.left_slider,50,0)
+        self.drag_drop_offset(Womentab.right_slider, -70, 0)
+
